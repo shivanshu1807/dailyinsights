7cc19803f15fcccc988e46926a6623e908dbf0c0
Shivanshu Singh <103367759+shivanshu1807@users.noreply.github.com>
2025-04-05 00:01:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,17 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
 import Home from "./components/Home";
-import './App.css'
-function App() {
-  return (
-    <Router>
-
-
-    
-    
-          <Navbar />
-          <div className="contain">
-            <Routes>
+impor
             <Route exact path="/" element={<Home/>}></Route>
             <Route exact path="/signup" element={<Signup/>} className=""></Route>
             <Route exact path="/login" element={<Login/>} className=""></Route>
