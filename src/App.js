8e0af961a1dd9b0a96e2f5675796b4c169c70ef8
Shivanshu Singh <103367759+shivanshu1807@users.noreply.github.com>
2025-04-05 00:03:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import React from "react";
-import Navbar from "./components/Navbar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Signup from "./components/Signup";
-import Login from "./components/Login";
-import Home from "./components/Home";
+import 
 impor
             <Route exact path="/" element={<Home/>}></Route>
             <Route exact path="/signup" element={<Signup/>} className=""></Route>
